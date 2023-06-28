f4adf8c8ea970a2754b823be2d22cbd06b0d0fb8
calico-c <harriethumby@aol.com>
2023-06-28 12:34:36 -0400

    changes made with css file 1

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -64,7 +64,8 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  {/* <label className="text-end me-1" htmlFor="cost" 
+                            style={{ marginLeft: '2rem' , size: 10}}>{currency}</label> */}
                     <input
                         required='required'
                         // type='number'
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
-
+import './ComponentStyles.css';
 const Currency = () => {
     const { budget,dispatch,expenses, remaining,currency } = useContext(AppContext);
     const [name, setName] = useState('');
@@ -12,30 +12,26 @@ const Currency = () => {
         })
 	}
 
-    const customStyles = {
-        option: ( { data, isDisabled, isFocused, isSelected }) => {
-        return {
-          backgroundColor: "#white"
-        };
-      }
-    };
+    const styles = {
+        hover_color: {
+          backgroundColor: "#f1f1f1",
+        },
+      };
+
     
     return (
-        <div className='alert alert-secondary'>  
-        <span>
-          <select 
-            className='btn btn-success dropdown-toggle'
-            id='currency'
-            style={{ marginLeft: '1rem' , backgroundColor:'#a5e2a0', color:'white', border: '#a5e2a0'}}
-            onChange={(event) =>changeCurrency(event)}>Currency ({currency})
-              <option style={{color:'black'}} defaultValue={currency}>Currency ({currency} Pound)</option>
-              <option style={{color:'black'}} value='£'>£ Pound</option>
-              <option style={{color:'black'}} value='$'>$ Dollar</option>
-              <option style={{color:'black'}} value='€'>€ Euro</option>
-              <option style={{color:"#5cb85c"}} value='₹'>₹ Rupee</option>
-          </select>
-          </span>
-        </div>
+
+		<div className='alert alert-secondary'>
+        <label style={{marginLeft: '1rem' , backgroundColor:'#a5e2a0', color:'white'}} >Currency (
+      <select name='hover_color'id="currency" onChange={(event)=>changeCurrency(event.target.value)} style={{backgroundColor:'#a5e2a0', color:'white'}}>
+        <option style={{color:'black'}} value="£">£ Pound</option>
+        <option style={{color:'black'}} value="$">$ Dollar</option>
+        <option style={{color:'black'}} value="€">€ Euro</option>
+        <option style={{color:'black'}} value="₹">₹ Rupee</option>
+      </select>	
+        )</label>
+    </div>
+
 	);
 };
 
